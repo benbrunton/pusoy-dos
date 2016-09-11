0f326da13c6bd7bc05d5e6184e7b0fce735411d0
Roy Brunton <notnurbyor@googlemail.com>
2016-09-11 17:10:14 +0100

    Add macro for five card trick building

--- src/game/player_move.rs
+++ src/game/player_move.rs
@@ -2,6 +2,10 @@ use cards::card::Card;
 use cards::types::*;
 use std::collections::HashMap;
 
+macro_rules! build_fct {
+    ($trick:ident, $cards:ident) => (Some(Move::FiveCardTrick(Trick::$trick($cards[0], $cards[1], $cards[2], $cards[3], $cards[4]))));
+}
+
 #[derive(Clone, Debug, PartialEq, Copy, PartialOrd)]
 /// Type of hand that can be played
 pub enum Move{
@@ -49,7 +53,7 @@ pub fn build_move(cards: Vec<Card>) -> Option<Move> {
 
 fn check_valid_pair(cards: Vec<Card>) -> Option<Move> {
 
-    if cards.len() == 2 && get_counts(cards.clone()).len() == 1 {
+    if get_counts(cards.clone()).len() == 1 {
         Some(Move::Pair(cards[0], cards[1]))
     } else {
         None
@@ -58,7 +62,7 @@ fn check_valid_pair(cards: Vec<Card>) -> Option<Move> {
 
 fn check_valid_prial(cards: Vec<Card>) -> Option<Move> {
 
-    if cards.len() == 3 && get_counts(cards.clone()).len() == 1 {
+    if get_counts(cards.clone()).len() == 1 {
         Some(Move::Prial(cards[0], cards[1], cards[2]))
     } else {
         None
@@ -69,11 +73,11 @@ fn check_valid_fct(cards: Vec<Card>) -> Option<Move> {
 
     let rank_count = get_counts(cards.clone());
     match rank_count.len() {
-        1 => build_five_of_a_kind(cards),
+        1 => build_fct!(FiveOfAKind, cards),
         2 => {
            match *rank_count.values().last().unwrap() {
-                3 | 2   => build_full_house(cards),
-                4 | 1   => build_four_of_a_kind(cards),
+                3 | 2   => build_fct!(FullHouse, cards),
+                4 | 1   => build_fct!(FourOfAKind, cards),
                 _       => None
 
            }
@@ -83,9 +87,9 @@ fn check_valid_fct(cards: Vec<Card>) -> Option<Move> {
             let straight = cards.iter().enumerate().all(|(i, &card)| i == 0 || card.previous_rank().is_some() && cards[i-1].rank == card.previous_rank().unwrap());
             let flush = cards.iter().all(|&card| card.suit == cards[0].suit);
             match (straight, flush) {
-                (true, true)    => build_straight_flush(cards),
-                (true, _)       => build_straight(cards),
-                (_, true)       => build_flush(cards),
+                (true, true)    => build_fct!(StraightFlush, cards),
+                (true, _)       => build_fct!(Straight, cards),
+                (_, true)       => build_fct!(Flush, cards),
                 _               => None
             }
         },
@@ -99,82 +103,3 @@ fn get_counts(cards: Vec<Card>) -> HashMap<Rank, usize> {
         acc
     })
 }
-
-fn build_four_of_a_kind(cards: Vec<Card>) -> Option<Move> {
-    match cards.len() {
-        5 => Some(Move::FiveCardTrick(
-                    Trick::FourOfAKind(
-                            cards[0],
-                            cards[1],
-                            cards[2],
-                            cards[3],
-                            cards[4]))),
-        _ => None
-    }
-}
-
-fn build_five_of_a_kind(cards: Vec<Card>) -> Option<Move> {
-    match cards.len() {
-        5 => Some(Move::FiveCardTrick(
-                    Trick::FiveOfAKind(
-                            cards[0],
-                            cards[1],
-                            cards[2],
-                            cards[3],
-                            cards[4]))),
-        _ => None
-    }
-}
-
-fn build_full_house(cards: Vec<Card>) -> Option<Move> {
-    match cards.len() {
-        5 => Some(Move::FiveCardTrick(
-                    Trick::FullHouse(
-                            cards[0],
-                            cards[1],
-                            cards[2],
-                            cards[3],
-                            cards[4]))),
-        _ => None
-    }
-}
-
-fn build_flush(cards: Vec<Card>) -> Option<Move> {
-    match cards.len() {
-        5 => Some(Move::FiveCardTrick(
-                    Trick::Flush(
-                            cards[0],
-                            cards[1],
-                            cards[2],
-                            cards[3],
-                            cards[4]))),
-        _ => None
-    }
-}
-
-fn build_straight(cards: Vec<Card>) -> Option<Move> {
-    match cards.len() {
-        5 => Some(Move::FiveCardTrick(
-                    Trick::Straight(
-                            cards[0],
-                            cards[1],
-                            cards[2],
-                            cards[3],
-                            cards[4]))),
-        _ => None
-    }
-}
-
-fn build_straight_flush(cards: Vec<Card>) -> Option<Move> {
-    match cards.len() {
-        5 => Some(Move::FiveCardTrick(
-                    Trick::StraightFlush(
-                            cards[0],
-                            cards[1],
-                            cards[2],
-                            cards[3],
-                            cards[4]))),
-        _ => None
-    }
-}
-
